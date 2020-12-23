43c8fb867ddf59031e5eb3f2d5f1b66be99d8c80
Carlos Aguilar <agustin3a94@gmail.com>
2020-12-22 18:39:21 -0600

    First commit

---
+++ src/main/java/com/learning/springwebapp/SpringCourseProject1Application.java
@@ -0,0 +1,13 @@
+package com.learning.springwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCourseProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCourseProject1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/springwebapp/SpringCourseProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.springwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourseProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
